d83f7ddb04a2afb22cbd0c2da8ffc53e5bd73e3c
Luis Diniz Do Carmo <81914129+LuisDoc@users.noreply.github.com>
2021-11-17 13:40:38 +0100

    Fixed: Sortieren und Filtern leisten nur noch einmaliges vorkommen auf Gruppieren Seite

--- resources/views/Main/viewGroup.blade.php
+++ resources/views/Main/viewGroup.blade.php
@@ -3,17 +3,18 @@
     <div class="container-fluid navcontainer">
         <nav class="navbar navbar-expand-lg navbar-dark back">
             <a class="navbar-brand" href="/Startseite">Tiktasks</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggler" aria-controls="navbarToggler" aria-expanded="false" aria-label="Toggle navigation">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggler"
+                aria-controls="navbarToggler" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarToggler">
                 <ul class="navbar-nav ms-auto">
-                    @if(auth()->user()->isAdmin())
+                    @if (auth()->user()->isAdmin())
                         <li class="nav-item">
                             <a class="nav-link" href="/Startseite">Systemverwaltung</a>
                         </li>
                     @endif
-                    @if(auth()->user()->isManager())
+                    @if (auth()->user()->isManager())
                         <li class="nav-item">
                             <a class="nav-link" href="/Startseite">Zuweisen</a>
                         </li>
@@ -35,67 +36,85 @@
 
 @section('content')
     <div>
-                @foreach($tasks as $task)
-                    @if($task->completed == false)
+        <div class="container">
+            <span id=erstell><a id=erstellen href="/Create-task"></a></span>
+            <span id=gruppe><a id=gruppieren href="/Group"></a></span>
+            <div class="btn-group">
+                <button class="btn btn-lg dropdown-toggle Sortbtn" type="button" data-bs-toggle="dropdown"
+                    aria-expanded="false">
+                    Sortieren nach
+                </button>
+                <ul class="dropdown-menu">
+                    <li><a class="dropdown-item" href="/SortbyNameAscGROUP">Titel aufsteigend</a></li>
+                    <li><a class="dropdown-item" href="/SortbyNameDescGROUP">Titel absteigend</a></li>
+                    <li><a class="dropdown-item" href="/SortbyDeadlineAscGROUP">Deadline aufsteigend</a></li>
+                    <li><a class="dropdown-item" href="/SortbyDeadlineDescGROUP">Deadline absteigend</a></li>
+                    <li><a class="dropdown-item" href="/SortbyDateAscGROUP">Erstellungdatum aufsteigend</a></li>
+                    <li><a class="dropdown-item" href="/SortbyDateDescGROUP">Erstellungdatum absteigend</a></li>
+                    <li><a class="dropdown-item" href="/SortbyPriorityAscGROUP">Priorität aufsteigend</a></li>
+                    <li><a class="dropdown-item" href="/SortbyPriorityDescGROUP">Priorität absteigend</a></li>
+                </ul>
+            </div>
+            <form class="form-inline filter" method="get" action="/searchGROUP">
+                @csrf
+                <input class="form-control mr-sm-2 filterinput" type="search" name="search" placeholder="Filtern nach"
+                    aria-label="Search">
+                <button class="btn btn-outline-success my-2 my-sm-0 searchbutton" type="submit"><i
+                        class="fas fa-search"></i></button>
+            </form>
+        </div>
+        @foreach ($tasks as $task)
+            @if ($task->completed == false)
                 <div class="container">
-                    <span id=erstell ><a id=erstellen href="/Create-task" ></a></span>
-                    <span id=gruppe ><a id=gruppieren href="/Group" ></a></span>
-                    <div class="btn-group">
-                        <button class="btn btn-lg dropdown-toggle Sortbtn" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        Sortieren nach
-                        </button>
-                        <ul class="dropdown-menu">
-                            <li><a class="dropdown-item" href="/SortbyNameAscGROUP">Titel aufsteigend</a></li>
-                            <li><a class="dropdown-item" href="/SortbyNameDescGROUP">Titel absteigend</a></li>
-                            <li><a class="dropdown-item" href="/SortbyDeadlineAscGROUP">Deadline aufsteigend</a></li>
-                            <li><a class="dropdown-item" href="/SortbyDeadlineDescGROUP">Deadline absteigend</a></li>
-                            <li><a class="dropdown-item" href="/SortbyDateAscGROUP">Erstellungdatum aufsteigend</a></li>
-                            <li><a class="dropdown-item" href="/SortbyDateDescGROUP">Erstellungdatum absteigend</a></li>
-                            <li><a class="dropdown-item" href="/SortbyPriorityAscGROUP">Priorität aufsteigend</a></li>
-                            <li><a class="dropdown-item" href="/SortbyPriorityDescGROUP">Priorität absteigend</a></li>
-                        </ul>
-                    </div>
-                    <form class="form-inline filter" method="get" action="/searchGROUP">
-                        @csrf
-                            <input class="form-control mr-sm-2 filterinput" type="search" name="search" placeholder="Filtern nach" aria-label="Search">
-                            <button class="btn btn-outline-success my-2 my-sm-0 searchbutton" type="submit"><i class="fas fa-search"></i></button>
-                    </form>
                     <div class="row task">
                         <div class="col-lg-11 col-md-11 col-sm-11">
                             <div class="card tabsize">
                                 <div class="card-header text-center aufgabenwrapper">
                                     <h4 class="card-title">
-                                        @if(!empty($task->deadline))
-                                            {{$date = date("d-m-Y H:i", strtotime($task->deadline))}}
+                                        @if (!empty($task->deadline))
+                                            {{ $date = date('d-m-Y H:i', strtotime($task->deadline)) }}
                                         @endif
                                     </h4>
                                 </div>
                                 <div class="card-body overflow-auto">
-                                    <p class="text">Erstellt am: {{$date = date("d-m-Y H:i", strtotime($task->created_at))}}<br><br></p>
-                                    <h4 class="card-title text-center">{{$task->title}}</h4><br>
-                                    <p class="text "><h5 class="card-title">Beschreibung</h5>{!!$task->description!!}</p>
-                                    <p class="text"><h5 class="card-title">Kommentare</h5>{!!$task->comment!!}<br><br></p>
-                                    <a class="btn btn-dark" type="button" name="button" href="/Startseite/{{$task->tasks_id}}/edit"><i class="fas fa-edit">Bearbeiten</i></a>
-                                    <a class="btn btn-warning" type="button" data-bs-toggle="modal" data-bs-target="#finish{{$task->tasks_id}}"><i class="fas fa-trash-alt"> Beenden</i></a>
-                                    <a class="btn btn-danger" type="button" data-bs-toggle="modal" data-bs-target="#deleteTask{{$task->tasks_id}}"><i class="fas fa-trash-alt"> Löschen</i></a>
-                                    <a class="btn btn-primary" type="button" name="button" href="/Startseite"><i class="fas fa-sync"> Aktualisieren</i></a>
+                                    <p class="text">Erstellt am:
+                                        {{ $date = date('d-m-Y H:i', strtotime($task->created_at)) }}<br><br></p>
+                                    <h4 class="card-title text-center">{{ $task->title }}</h4><br>
+                                    <p class="text ">
+                                    <h5 class="card-title">Beschreibung</h5>{!! $task->description !!}</p>
+                                    <p class="text">
+                                    <h5 class="card-title">Kommentare</h5>{!! $task->comment !!}<br><br></p>
+                                    <a class="btn btn-dark" type="button" name="button"
+                                        href="/Startseite/{{ $task->tasks_id }}/edit"><i
+                                            class="fas fa-edit">Bearbeiten</i></a>
+                                    <a class="btn btn-warning" type="button" data-bs-toggle="modal"
+                                        data-bs-target="#finish{{ $task->tasks_id }}"><i class="fas fa-trash-alt">
+                                            Beenden</i></a>
+                                    <a class="btn btn-danger" type="button" data-bs-toggle="modal"
+                                        data-bs-target="#deleteTask{{ $task->tasks_id }}"><i class="fas fa-trash-alt">
+                                            Löschen</i></a>
+                                    <a class="btn btn-primary" type="button" name="button" href="/Startseite"><i
+                                            class="fas fa-sync"> Aktualisieren</i></a>
                                 </div>
-                                @if(!empty($task->deadline))
+                                @if (!empty($task->deadline))
                                     <div class="container-fluid">
-                                        <a class="badge rounded-pill bg-info" href="{{$task->calendarICS}}">Ics Datei</a>
-                                        <a class="badge rounded-pill bg-info" href="{{$task->calendarGoogle}}">Google Calendar</a>
-                                        <a class="badge rounded-pill bg-info" href="{{$task->calendarWebOutlook}}">WebOutlook Calendar</a>
+                                        <a class="badge rounded-pill bg-info" href="{{ $task->calendarICS }}">Ics
+                                            Datei</a>
+                                        <a class="badge rounded-pill bg-info" href="{{ $task->calendarGoogle }}">Google
+                                            Calendar</a>
+                                        <a class="badge rounded-pill bg-info"
+                                            href="{{ $task->calendarWebOutlook }}">WebOutlook Calendar</a>
                                     </div>
                                 @endif
                                 <div class="card-footer text-muted text-center">
-                                    {{$totalDuration = Carbon\Carbon::now()->diffForHumans($task->deadline)}}
+                                    {{ $totalDuration = Carbon\Carbon::now()->diffForHumans($task->deadline) }}
                                 </div>
                             </div>
 
 
                         </div>
                         <div class="col-lg-1 col-md-1 col-sm-1">
-                            @if($task->priority==1)
+                            @if ($task->priority == 1)
                                 <div class="card priority1 tabsize"></div>
                             @elseif($task->priority==2)
                                 <div class="card priority2 tabsize"></div>
@@ -110,42 +129,48 @@
                     </div>
                 </div>
                 <!-- Modal Delete Task-->
-                <div class="modal fade" id="deleteTask{{$task->tasks_id}}" tabindex="-1" aria-labelledby="deleteTaskLabel" aria-hidden="true">
+                <div class="modal fade" id="deleteTask{{ $task->tasks_id }}" tabindex="-1"
+                    aria-labelledby="deleteTaskLabel" aria-hidden="true">
                     <div class="modal-dialog">
                         <div class="modal-content">
                             <div class="modal-header">
                                 <h5 class="modal-title" id="deleteTaskLabel">Aufgabe löschen</h5>
-                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                    aria-label="Close"></button>
                             </div>
                             <div class="modal-body">
                                 Möchten sie die Aufgabe wirklich löschen?
                             </div>
                             <div class="modal-footer">
                                 <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Abbrechen</button>
-                                <a type="button" class="btn btn-danger" href="/Startseite/{{$task->tasks_id}}/delete" >Löschen</a>
+                                <a type="button" class="btn btn-danger"
+                                    href="/Startseite/{{ $task->tasks_id }}/delete">Löschen</a>
                             </div>
                         </div>
                     </div>
                 </div>
                 <!-- Modal finish Task-->
-                <div class="modal fade" id="finish{{$task->tasks_id}}" tabindex="-1" aria-labelledby="finishLabel" aria-hidden="true">
+                <div class="modal fade" id="finish{{ $task->tasks_id }}" tabindex="-1" aria-labelledby="finishLabel"
+                    aria-hidden="true">
                     <div class="modal-dialog">
                         <div class="modal-content">
                             <div class="modal-header">
                                 <h5 class="modal-title" id="finishLabel">Aufgabe beenden</h5>
-                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                    aria-label="Close"></button>
                             </div>
                             <div class="modal-body">
                                 Möchten sie die Aufgabe abschliessen?
                             </div>
                             <div class="modal-footer">
                                 <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Abbrechen</button>
-                                <a type="button" class="btn btn-danger" href="/Startseite/{{$task->tasks_id}}/complete" >Beenden</a>
+                                <a type="button" class="btn btn-danger"
+                                    href="/Startseite/{{ $task->tasks_id }}/complete">Beenden</a>
                             </div>
                         </div>
                     </div>
                 </div>
-                @endif
-                @endforeach
+            @endif
+        @endforeach
     </div>
 @endsection
