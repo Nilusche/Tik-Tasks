3ed753780888f50c1f94d00b97969db4e8daaaa2
Nilusche <niluschel@outlook.de>
2021-12-09 11:07:19 +0100

    Create validation changes

--- resources/views/Main/createTask.blade.php
+++ resources/views/Main/createTask.blade.php
@@ -2,15 +2,6 @@
 
 @section('content')
     <div class="container">
-        @if ($errors->any())
-            <div class="alert alert-danger">
-                <ul class="list-group">
-                    @foreach ($errors->all() as $error)
-                        <li class="list-group-item">{{ $error }}</li>
-                    @endforeach
-                </ul>
-            </div>
-        @endif
         <style>*,
             *:before,
             *:after{
@@ -22,7 +13,12 @@
             textarea::placeholder{color: black;}
             input[type=number]::-webkit-inner-spin-button {
                 -webkit-appearance: none;
-            } </style>
+            } 
+            .danger{
+                color:red;
+            }
+            
+            </style>
         <head>
             <title>Aufgabe erstellen/ändern</title>
 
@@ -45,18 +41,36 @@
             <div class="teil_a_e">
                 <div class="neuer_abschnitt">
                     <label class="labelinput_a_e" for="title">Titel *</label>
-                    <input class="input_a_e" type="text"  id="title" name="title"  value="{{old('title')}}"placeholder="Titel der Aufgabe" required>
+                    <input class="input_a_e  @error('title') is-invalid @enderror" type="text"  id="title" name="title"  value="{{old('title')}}"placeholder="Titel der Aufgabe">
+                    @error('title')
+                        <span class="invalid-feedback" role="alert">
+                            <strong class="danger">{{ $message }}</strong>
+                        </span>
+                    @enderror
 
 
                     <label class="labelinput_a_e" for="deadline">Deadline</label>
-                    <input class="input_a_e" type="datetime-local" id="deadline" name="deadline" value="{{old('deadline')}}">
-
+                    <input class="input_a_e  @error('deadline') is-invalid @enderror" type="datetime-local" id="deadline" name="deadline" value="{{old('deadline')}}">
+                    @error('deadline')
+                        <span class="invalid-feedback" role="alert">
+                            <strong class="danger">{{ $message }}</strong>
+                        </span>
+                    @enderror
 
                     <label class="labelinput_a_e" for="description">Beschreibung</label>
-                    <textarea class="inputtext_a_e"  class="form-control" name="description" placeholder="Beschreibung" id="description" cols="50" rows="9">{{old('description')}}</textarea>
-
+                    <textarea class="inputtext_a_e  @error('description') is-invalid @enderror"  class="form-control" name="description" placeholder="Beschreibung" id="description" cols="50" rows="9">{{old('description')}}</textarea>
+                    @error('description')
+                        <span class="invalid-feedback" role="alert">
+                            <strong class="danger">{{ $message }}</strong>
+                        </span>
+                    @enderror
                     <label class="labelinput_a_e" for="comment">Kommentar</label>
-                    <input class="input_a_e" type="text"  id="comment" name="comment" value="{{old('comment')}}" placeholder="Kommentar">
+                    <input class="input_a_e @error('comment') is-invalid @enderror" type="text"  id="comment" name="comment" value="{{old('comment')}}" placeholder="Kommentar">
+                    @error('comment')
+                        <span class="invalid-feedback" role="alert">
+                            <strong class="danger">{{ $message }}</strong>
+                        </span>
+                    @enderror
                 </div><div>
                     <label class="labelinput_a_e" for="visibility" >Sichtbarkeit</label>
                     <select class="boxs" id="visibility" name="visibility" value="{{old('visibility')}}" aria-label="Default select example">
@@ -83,8 +97,12 @@
                     </select>
 
                     <label class="labelinput_a_e" for="effort" >Geschätzter aufwand (in Stunden)</label>
-                    <input class="input_a_e" type="number" id="effort" name="effort" min="0" value="{{old('effort')}}" placeholder="3.5">
-
+                    <input class="input_a_e @error('effort') is-invalid @enderror" type="number" id="effort" name="effort" min="0" value="{{old('effort')}}" placeholder="3.5">
+                    @error('effort')
+                        <span class="invalid-feedback" role="alert">
+                            <strong class="danger">{{ $message }}</strong>
+                        </span>
+                    @enderror
                     <p>Die mit * markierten Felder sind Pflichteingaben</p>
                 </div>
             </div>
