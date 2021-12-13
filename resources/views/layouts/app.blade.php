3558e62fa2b8d28f737c640169bd67bbcf1cea09
Luis Diniz Do Carmo <81914129+LuisDoc@users.noreply.github.com>
2021-12-13 14:30:57 +0100

    Notifcation Bell added in Navigation-Bar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,30 +1,35 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="/css/styles.css">
     <link rel="stylesheet" href="/css/loader.css">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
+    </script>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat+Alternates:wght@500&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Audiowide&display=swap" rel="stylesheet">
     <script src="https://kit.fontawesome.com/b90fa0e727.js" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.7.14/css/bootstrap-datetimepicker.min.css">
-    <link href="https://cdn.rawgit.com/harvesthq/chosen/gh-pages/chosen.min.css" rel="stylesheet"/>
+    <link rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.7.14/css/bootstrap-datetimepicker.min.css">
+    <link href="https://cdn.rawgit.com/harvesthq/chosen/gh-pages/chosen.min.css" rel="stylesheet" />
     <link rel="stylesheet" href="https://unpkg.com/aos@2.3.1/dist/aos.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Chakra+Petch:wght@300&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/alertifyjs@1.13.1/build/css/alertify.min.css"/>
+    <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/alertifyjs@1.13.1/build/css/alertify.min.css" />
     <script>
         window.Laravel = {!! json_encode([
-            'user' => auth()->check() ? auth()->user()->id : null,
-        ]) !!};
+    'user' => auth()->check() ? auth()->user()->id : null,
+]) !!};
     </script>
 
     <title>TikTasks</title>
@@ -32,10 +37,11 @@
     @yield('scripts')
 
 </head>
+
 <body>
     @include('sweetalert::alert', ['cdn' => "https://cdn.jsdelivr.net/npm/sweetalert2@9"])
     <div id=app>
-        
+
     </div>
     <div class="container-fluid navcontainer">
         <nav class="navbar navbar-expand-lg navbar-dark back">
@@ -43,11 +49,23 @@
                 <img class="TikTaskPicture" src="/sources/logo_white.png" alt="TikTasksPicture" width=auto height="150">
             </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggler"
-                    aria-controls="navbarToggler" aria-expanded="false" aria-label="Toggle navigation">
+                aria-controls="navbarToggler" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarToggler">
                 <ul class="navbar-nav ms-auto">
+                    <li class="nav-item">
+                        <a class="nav-item" href="/UserNotifications" data-toggle="tooltip" data-placement="top"
+                            title="Benachrichtigungen">
+                            <i class="fas fa-bell icon">
+                                <span class="notifactionBadge">
+                                    105
+                                </span>
+                            </i>
+
+                        </a>
+
+                    </li>
                     @if (auth()->user()->isAdmin())
                         <li class="nav-item">
                             <a class="nav-link" href="/Systempanel">Systemverwaltung</a>
@@ -58,6 +76,7 @@
                             <a class="nav-link" href="/Assign">Zuweisen</a>
                         </li>
                     @endif
+
                     <li class="nav-item">
                         <a class="nav-link" href="/Settings">Einstellungen</a>
                     </li>
@@ -67,40 +86,41 @@
                     <li class="nav-item">
                         <a class="nav-link" href="/logout">Ausloggen</a>
                     </li>
+
                 </ul>
             </div>
         </nav>
     </div>
 
-    @if(session()->has('success'))
-    <div class=" container alert alert-success">
-        {{session()->get('success')}}
-    </div>
+    @if (session()->has('success'))
+        <div class=" container alert alert-success">
+            {{ session()->get('success') }}
+        </div>
     @elseif(session()->has('error'))
-    <div class=" container alert alert-danger">
-        {{session()->get('error')}}
-    </div>
+        <div class=" container alert alert-danger">
+            {{ session()->get('error') }}
+        </div>
     @endif
     @yield('content')
     <footer class="text-center text-lg-start">
-      <div class="text-center p-3 foot">
-          Tik Tasks 2021: This is a simple To-do-list Tool and designed for educational purposes only
-      </div>
+        <div class="text-center p-3 foot">
+            Tik Tasks 2021: This is a simple To-do-list Tool and designed for educational purposes only
+        </div>
     </footer>
 
 
-    
 
-    <script src="{{asset('js/app.js')}}"></script>
+
+    <script src="{{ asset('js/app.js') }}"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
     <script src="https://cdn.rawgit.com/harvesthq/chosen/gh-pages/chosen.jquery.min.js"></script>
     <script>
-    $(".chosen-select").chosen({
-      no_results_text: "Oops, nothing found!"
-    })
+        $(".chosen-select").chosen({
+            no_results_text: "Oops, nothing found!"
+        })
     </script>
 
-    
+
 
     <!-- loader-->
     <div class="loader-wrapper">
@@ -125,7 +145,7 @@
         </div>
     </div>
     <script>
-        $(window).on("load",function(){
+        $(window).on("load", function() {
             $(".loader-wrapper").fadeOut("slow");
         })
     </script>
@@ -133,31 +153,36 @@
     <script src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/8.6/highlight.min.js"></script>
     <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
     <script>
-			AOS.init({
-				easing: 'ease-out-back',
-				duration: 700
-			});
-	</script>
+        AOS.init({
+            easing: 'ease-out-back',
+            duration: 700
+        });
+    </script>
     <script src="//cdn.jsdelivr.net/npm/alertifyjs@1.13.1/build/alertify.min.js"></script>
     <script>
-        (function(){
+        (function() {
 
-        var originallog = console.log;
+            var originallog = console.log;
 
-        console.log = function(txt) {
-            // Do really interesting stuff
-            //alert("Neue Benachrichtigung, Aufgabe abgelaufen");
-            alertify
-            .alert("Sie haben gerade eine neue Benachrichtigung erhalten. Eine Aufgabe nähert sich der Deadline.", function(){
-                alertify.message('OK');
-            }).set({title:"Benachrichtigung"});
+            console.log = function(txt) {
+                // Do really interesting stuff
+                //alert("Neue Benachrichtigung, Aufgabe abgelaufen");
+                alertify
+                    .alert(
+                        "Sie haben gerade eine neue Benachrichtigung erhalten. Eine Aufgabe nähert sich der Deadline.",
+                        function() {
+                            alertify.message('OK');
+                        }).set({
+                        title: "Benachrichtigung"
+                    });
 
-            originallog.apply(console, arguments);
-        }
+                originallog.apply(console, arguments);
+            }
 
         })();
     </script>
 
-    
+
 </body>
+
 </html>
