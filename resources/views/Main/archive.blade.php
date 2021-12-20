3989314dd571b58a0ace6cb5b122a7bc3ff813c8
Nilusche <niluschel@outlook.de>
2021-12-20 13:30:31 +0100

    modal for archiving tasks

--- resources/views/Main/archive.blade.php
+++ resources/views/Main/archive.blade.php
@@ -10,7 +10,7 @@
     </div>
     <div class="container">
 
-        <span id=leere><a id=leeren href="/deleteArchive"></a></span>
+        <span id=leere><a id=leeren data-bs-toggle="modal" data-bs-target="#delarchive" href=""></a></span>
         @foreach ($TaskUserPairs as $TaskUserPair)
             @if ($TaskUserPair->users_id == auth()->user()->id)
                 @foreach ($tasks as $task)
@@ -39,7 +39,7 @@
                                             @endif
                                     </ul>
                                 </div>
-                                <div class="description">
+                                <div class="description" id="desc">
                                     @if ($task->deadline)
                                         <h1 class="h1-color mb-4">
                                             <div class="Deadline-title"><i class="far fa-calendar-alt"></i>&nbsp;DEADLINE:
@@ -63,7 +63,7 @@
                                     <p class="read-more">
                                         <a type="button" data-toggle="collapse" id="open"
                                             data-target="#collapseExample{{ $task->id }}" aria-expanded="false"
-                                            aria-controls="collapseExample{{ $task->id }}">Mehr anzeigen</a>
+                                            aria-controls="collapseExample{{ $task->id }}">auf-/zuklappen</a>
                                     </p>
 
                                     <div class="collapse" id="collapseExample{{ $task->id }}">
@@ -89,4 +89,21 @@
             @endif
         @endforeach
     </div>
+    <div class="modal fade" id="delarchive" tabindex="-1" aria-labelledby="deleteLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="deleteLabel">Archiv löschen</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal"aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        Möchten sie alle Aufgaben aus dem Archiv löschen?
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary"data-bs-dismiss="modal">Abbrechen</button>
+                        <a type="button" class="btn btn-danger"href="/deleteArchive">Löschen</a>
+                    </div>
+                </div>
+            </div>
+        </div>
 @endsection
