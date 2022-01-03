95ddc04670518b4be549b2f8b56ea112418f5198
Luis Diniz Do Carmo <81914129+LuisDoc@users.noreply.github.com>
2022-01-03 14:55:53 +0100

    Update Drop Down Menu on Assign Tasks

--- resources/views/Main/assign.blade.php
+++ resources/views/Main/assign.blade.php
@@ -2,24 +2,25 @@
 
 @section('content')
     @if (auth()->user()->isManager())
-        <div class ="container">
+        <div class="container">
             <div class="btn btn-secondary position-relative">
-                <h1>{{__('manager.assign')}}</h1>
+                <h1>{{ __('manager.assign') }}</h1>
             </div>
         </div>
 
-        <div class ="container">
-            <div class ="assignpanel">
-                <img src ="sources/assignTask.svg" alt ="assignTask Picture">
+        <div class="container">
+            <div class="assignpanel">
+                <img src="sources/assignTask.svg" alt="assignTask Picture">
                 @if ($TaskUserPairs->first())
                     <form id="zuweisen" action="/assignTasks" method="POST">
                         @csrf
-                        <ul style="overflow-Y: scroll; height: 120px; width: 70%;">
-                            @foreach($users as $user)
-                                <li style="margin-bottom:-2px">{{$user->email}}</li>
+                        <label class="labelinput_b" for="Benutzer">{{ __('admin.selectuser') }}</label>
+                        <select name="operator" id="Benutzer">
+                            <option value="" selected></option>
+                            @foreach ($users as $user)
+                                <option value="{{ $user->email }}">{{ $user->name }} - {{ $user->email }}</option>
                             @endforeach
-                        </ul>
-                        <input id="zuweisen-input" type="text" name="operator" placeholder="{{__('admin.worker')}} {{__('profile.email')}}">
+                        </select>
                         <div id="zuweisen-list">
                             @foreach ($TaskUserPairs as $TaskUserPair)
                                 @if ($TaskUserPair->users_id == auth()->user()->id)
@@ -28,7 +29,7 @@
                                             <li id="zuweisen-item" class="list-group-item">
                                                 <label>
                                                     <input class="form-check-input me-1" name="tasks[]" type="checkbox"
-                                                           value="{{ $task->id }}" aria-label="...">
+                                                        value="{{ $task->id }}" aria-label="...">
                                                     {{ $task->title }}
                                                 </label>
                                             </li>
@@ -38,12 +39,13 @@
                             @endforeach
                         </div>
                         <div>
-                            <input id="zuweisenbutton" class="btn btn-secondary" type="submit" name="send" value="{{__('manager.send')}}">
+                            <input id="zuweisenbutton" class="btn btn-secondary" type="submit" name="send"
+                                value="{{ __('manager.send') }}">
                         </div>
                     </form>
                 @else
                     <div class="container">
-                        <h4 class="EmptyWebsite">{{__('manager.notask')}}</h4>
+                        <h4 class="EmptyWebsite">{{ __('manager.notask') }}</h4>
                     </div>
                 @endif
             </div>
